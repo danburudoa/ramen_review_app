98cbe562f3c55786cc0c15bffdd8e3a164d9d129
danburudoa <yumamukai515@gmail.com>
2022-09-13 17:32:12 +0900

    不要なファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_12_033814) do
+ActiveRecord::Schema.define(version: 2022_09_12_100713) do
+
+  create_table "ramen_shops", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "username"
